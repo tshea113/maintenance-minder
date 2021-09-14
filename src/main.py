9550ac9c0cf0b1bd680b380b0a76b72b07a8bdaf
tshea <tylershea12@gmail.com>
2021-09-13 22:14:45 -0500

    Basic service query added

--- src/main.py
+++ src/main.py
@@ -1,9 +1,16 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import Session
 from models import Car, Service, Base
+from datetime import date, datetime
 
+# -----------------------------------------------------------------------------------------------------------------
+#
+# Database operations
+#
+# -----------------------------------------------------------------------------------------------------------------
 def open_db(dbName):
-  engine = create_engine('sqlite:///' + dbName, echo=True)
+  engine = create_engine('sqlite:///' + dbName)
+  
   return engine
 
 def close_db(db):
@@ -13,21 +20,67 @@ def close_db(db):
 def create_tables(engine):
   Base.metadata.create_all(engine)
 
-def add_car(engine, newMake, newModel, newYear):
+def add_car(session, newMake, newModel, newYear):
   newCar = Car(make=newMake, model=newModel, year=newYear)
+  session.add(newCar)
+  
+  return newCar
 
+def add_service(session, newCar, newName, newDescription, newDate, newMileage):
+  newService = Service(name=newName, description=newDescription, date=newDate, mileage=newMileage, car=newCar)
+  session.add(newService)
+  
+  return newService
+
+def get_cars(session):
+  cars = session.query(Car).order_by(Car.car_id)
+  
+  return cars
+
+def get_services(session, car):
+  services = session.query(Service).where(Service.car == car).order_by(Service.date)
+  
+  return services
+
+# -----------------------------------------------------------------------------------------------------------------
+#
+# User operations
+#
+# -----------------------------------------------------------------------------------------------------------------
+
+def view_services():
   with Session(engine) as session:
     with session.begin():
-      session.add(newCar)
-    # inner context calls session.commit(), if there were no exceptions
-  # outer context calls session.close()
+      cars = get_cars(session)
+
+      print("Which car do you want to view?")
+      for index, car in enumerate(cars):
+        print(str(index+1) + '. ' + str(car.year) + ' ' + car.make + ' ' + car.model)
+      
+      userInput = input()
+
+      services = get_services(session, cars[int(userInput)-1])
+
+      for service in services:
+        print(service.name + ' ' + service.date.strftime('%m/%d/%Y') + ' ' + str(service.mileage))
+
+      print()
 
 if __name__ == "__main__":
-  # an Engine, which the Session will use for connection resources
+  # Initializes the database if it doesn't already exist
   engine = open_db('maintenance.db')
   create_tables(engine)
 
-  add_car(engine, 'Mazda', 'Miata', 1993)
+  while True:
+    print('Select an option:\n1. View Service History\n2. Exit')
+    userInput = input()
+    
+    if userInput[0] == '1': view_services()
+    elif userInput[0] == '2': break
 
-  # our_car = session.query(Car).filter_by(make='Mazda').first()
-  # print(our_car.car_id)
+  # with Session(engine) as session:
+  #   with session.begin():
+
+  #     currCar = get_cars(session)[0]
+
+  #     add_service(session, currCar, 'Tires', 'Nitto', date(1901, 2, 1), 70000)
